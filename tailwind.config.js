2d133b303b86368ca352985714d3f487c5ac7aba
“WangYingJu” <iriswang1091@gmail.com>
2024-11-18 19:12:03 +0800

    feat(utilities): add color style utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
+import colors from 'tailwindcss/colors'
 
 export default {
   content: [
@@ -16,6 +10,23 @@ export default {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      gray: {
+        ...colors.gray,
+        'input-bg': '#F5F7FA',
+        250: '#E8EDF3'
+      }
+    }
   },
   variants: {
     extend: {},
