e426508a5bcf41f4f42ce0dbadfff04f198780c3
“WangYingJu” <iriswang1091@gmail.com>
2024-11-21 19:47:31 +0800

    feat: add TopicDetailsPage layout.

---
+++ src/components/BreadcrumbNav.vue
@@ -0,0 +1,10 @@
+<script setup>
+//
+</script>
+
+<template>
+  <section class="mb-5">
+    <h1 class="text-sm text-gray-450 focus:text-primary-blue hover:text-primary-blue py-5px me-5">首頁>話題詳情頁
+    </h1>
+  </section>
+</template>
--- src/components/HotTopicsList.vue
+++ src/components/HotTopicsList.vue
@@ -7,19 +7,19 @@
     <h3 class="text-sm font-bold">熱門話題</h3>
     <ul>
       <li class="pt-5 pb-3 border-b hover:text-primary-blue">
-        <a href="#">AI 技術對工作市場的影響</a>
+        <RouterLink to="/topic">AI 技術對工作市場的影響</RouterLink>
       </li>
       <li class="pt-5 pb-3 border-b hover:text-primary-blue">
-        <a href="#">AI 2025 年奧斯卡入圍名單揭曉</a>
+        <RouterLink to="/topic">AI 2025 年奧斯卡入圍名單揭曉</RouterLink>
       </li>
       <li class="pt-5 pb-3 border-b hover:text-primary-blue">
-        <a href="#">AI 如何有效減壓？專家分享 5 個小技巧</a>
+        <RouterLink to="/topic">AI 如何有效減壓？專家分享 5 個小技巧</RouterLink>
       </li>
       <li class="pt-5 pb-3 border-b hover:text-primary-blue">
-        <a href="#">AI 探索宇宙的終極秘密</a>
+        <RouterLink to="/topic">AI 探索宇宙的終極秘密</RouterLink>
       </li>
       <li class="pt-5 pb-3 hover:text-primary-blue">
-        <a href="#">AI 新手健身指南：如何開始健身之旅？</a>
+        <RouterLink to="/topic">AI 新手健身指南：如何開始健身之旅？</RouterLink>
       </li>
     </ul>
   </div>
