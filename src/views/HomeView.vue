b1218fe61fde969352600745559aa487e6eaf357
Iris Wang <92594580+WangYingJu@users.noreply.github.com>
2024-11-23 01:22:02 +0800

    Feat/homepage layout (#2)
    * feat: add header layout
    
    * feat(utilities): add color style utilities
    
    * fix: 1. Delete some files and change routerlink to avatorview & homeview. 2. Set new utilities for container, spacing, color... 3. Add Unread Notification Badge for Member Icon.
    
    * feat: add homeview layout
    
    * fix: multiple tag syntax issues, format all files, and ensure proper newline characters.
    
    * fix: format all files
    
    ---------
    
    Co-authored-by: “WangYingJu” <“iriswang1091@gmail.com”>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HotTopicQuickAdd from '@/components/HotTopicQuickAdd.vue'
+import HotTopicsList from '@/components/HotTopicsList.vue'
+import SidebarAd from '@/components/SidebarAd.vue'
+import TopicList from '@/components/TopicList.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="container container-customizing-1060 flex justify-between my-[30px]">
+    <SidebarAd />
+    <TopicList />
+    <div class="right-sidebar">
+      <HotTopicQuickAdd />
+      <HotTopicsList />
+    </div>
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,43 @@
 /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
+import colors from 'tailwindcss/colors'
 
 export default {
-  purge: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  // purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '30px': '30px',
+        4.5: '18px'
+      }
+    },
+    container: {
+      center: true,
+      screens: {
+        'customizing-1060': '1060px',
+      }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      gray: {
+        ...colors.gray,
+        'input-bg': '#F5F7FA',
+        250: '#E8EDF3',
+        350: '#575757',
+        450: '#6B7280'
+      },
+      'primary-blue': '#3466F6',
+    }
   },
   variants: {
     extend: {},
