99c502014668acc0ea747fc46f632c4d0f19901c
Iris Wang <92594580+WangYingJu@users.noreply.github.com>
2025-01-22 16:30:40 +0800

    Feat/topic comments (#14)
    * feat: add comment submit button UI.
    
    * feat: integrate GET /topics/:id/comments API.
    
    * feat: add comment posting API for topics.
    
    * docs: add comments for watch function
    
    ---------
    
    Co-authored-by: “WangYingJu” <“iriswang1091@gmail.com”>

--- src/components/CommentCard.vue
+++ src/components/CommentCard.vue
@@ -1,27 +1,38 @@
 <script setup>
-//
+import timeToNow from '@/time'
+
+const props = defineProps({
+  commentsList: {
+    type: Array,
+    required: true,
+  },
+})
 </script>
 
 <template>
-  <div class="flex mb-5">
-    <!-- 留言者頭像 -->
-    <img
-      src="../assets/member.png"
-      alt="User Avatar"
-      class="w-10 h-10 object-cover rounded-full me-2"
-    />
-    <div>
-      <!-- 留言者名稱、時間 -->
-      <div class="flex mb-2">
-        <p class="text-sm font-medium me-3">ferdi sahin</p>
-        <time class="text-sm text-gray-450">30 秒前</time>
+  <ul>
+    <li v-for="comment in props.commentsList" :key="comment.id" class="flex mb-5">
+      <!-- 留言者頭像 -->
+      <img
+        :src="comment.user_pic"
+        alt="User Avatar"
+        class="w-10 h-10 object-cover rounded-full me-2"
+      />
+      <div>
+        <!-- 留言者名稱、時間 -->
+        <div class="flex mb-2">
+          <p class="text-sm font-medium me-3">{{ comment.user_name }}</p>
+          <time class="text-sm text-gray-450">{{ timeToNow(comment.updated_at) }}</time>
+        </div>
+        <!-- 留言內容 -->
+        <p class="text-sm text-gray-650 mb-2">
+          {{ comment.content }}
+        </p>
+        <!-- 對留言的操作 -->
+        <button type="button" class="text-[13px] font-medium text-gray-450">
+          讚({{ comment.likes }})
+        </button>
       </div>
-      <!-- 留言內容 -->
-      <p class="text-sm text-gray-650 mb-2">
-        上次去金澤跟北陸，遇到好多講中文的，在異鄉沒想到還能遇到這麼多台灣人跟其他講中文的朋友
-      </p>
-      <!-- 對留言的操作 -->
-      <button type="button" class="text-[13px] font-medium text-gray-450">讚(2)</button>
-    </div>
-  </div>
+    </li>
+  </ul>
 </template>
--- src/components/CommentSection.vue
+++ src/components/CommentSection.vue
@@ -1,16 +1,62 @@
 <script setup>
+import { ref, watch } from 'vue'
+import CommentCard from './CommentCard.vue'
+import { fetchComments } from '@/apis/getComments'
+import { createComment } from '@/apis/postComment'
+
+const props = defineProps({
+  topic: {
+    type: Object,
+    required: true,
+  },
+})
+
+const commentsList = ref([])
+const getCommentsList = async (id) => {
+  try {
+    const res = await fetchComments(id)
+    commentsList.value = res
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const tempComment = ref('')
+
+const addComment = async () => {
+  try {
+    if (tempComment.value) {
+      await createComment(props.topic.id, tempComment.value)
+      alert('留言成功')
+      tempComment.value = ''
+      await getCommentsList(props.topic.id)
+    } else {
+      alert('請輸入留言內容')
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// 點擊熱門話題的時候父元件有 watch router.query.id 就會去重新拿話題資料，所以變數 topicDetail 會跟著更新，所以需要 watch props.topic
+watch(
+  () => props.topic,
+  () => {
+    getCommentsList(props.topic.id)
+  },
+)
+
 function adjustHeight(event) {
   // 重設textarea高度，防止高度不斷累加
   event.target.style.height = 'auto'
   // 設定textarea為自動增長的高度
   event.target.style.height = `${event.target.scrollHeight}px`
 }
-import CommentCard from './CommentCard.vue'
 </script>
 
 <template>
   <section class="border rounded border-gray-250 bg-white p-10 mb-4">
-    <h3 class="text-lg font-bold mb-[30px]">留言 (3)</h3>
+    <h3 class="text-lg font-bold mb-[30px]">留言 ({{ props.topic.comments }})</h3>
     <!-- 寫下留言 -->
     <div class="flex mb-5">
       <img
@@ -20,20 +66,22 @@ import CommentCard from './CommentCard.vue'
       />
       <div class="w-full min-h-20 border-2 rounded border-gray-250 bg-white p-3 text-wrap">
         <textarea
-          placeholder="寫下你的留言..."
+          v-model="tempComment"
+          placeholder="寫下你的留言...(英文字元280個、中文140字)"
           class="w-full max-h-52 resize-none overflow-x-hidden focus:outline-none text-base leading-none text-gray-550"
           rows="5"
-          maxlength="100"
+          maxlength="280"
           @input="adjustHeight"
         ></textarea>
+        <div class="flex justify-end">
+          <button type="button" @click="addComment" class="text-base leading-5 text-primary-blue">
+            送出留言
+          </button>
+        </div>
       </div>
     </div>
     <!-- 顯示已留言則數 -->
-    <ul>
-      <li>
-        <CommentCard />
-      </li>
-    </ul>
+    <CommentCard :commentsList="commentsList" />
   </section>
   <button type="button" class="block mx-auto text-xs text-primary-blue">載入更多留言</button>
 </template>
