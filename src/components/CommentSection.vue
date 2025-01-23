63a8b96f85a12a5b86300d6bf71d01d06840328d
“WangYingJu” <iriswang1091@gmail.com>
2025-01-23 11:18:15 +0800

    refactor: update character limit rules for comment section.

--- src/components/CommentSection.vue
+++ src/components/CommentSection.vue
@@ -23,6 +23,7 @@ const getCommentsList = async (id) => {
 
 const tempComment = ref('')
 const charCount = ref(0)
+const maxLength = 280
 
 const addComment = async () => {
   try {
@@ -48,13 +49,13 @@ watch(
   },
 )
 
-// unicode範圍 包含中日韓文字
-// match() 會返回陣列
-// 因為英文每兩個字母為一個字元，所以要 Math.ceil() 會將小數點後的數字無條件進位
 watch(tempComment, (newVal) => {
-  const chineseCharCount = (newVal.match(/[\u2e80-\u9fff]/g) || []).length
-  const englishCharCount = newVal.length - chineseCharCount
-  charCount.value = chineseCharCount + Math.ceil(englishCharCount / 2)
+  // unicode範圍：\u2e80-\u9fff 為中日韓文字 \uff00-\uffef 為全形符號
+  // match() 會返回陣列
+  const chineseFullCharCount = (newVal.match(/[\u2e80-\u9fff\uff00-\uffef]/g) || []).length
+  const englishHalfCharCount = newVal.length - chineseFullCharCount
+  // 中文與全形符號佔 2 字元，英文與半形符號佔 1 字元
+  charCount.value = chineseFullCharCount * 2 + englishHalfCharCount
 })
 </script>
 
@@ -74,11 +75,17 @@ watch(tempComment, (newVal) => {
           placeholder="寫下你的留言...(英文字元280個、中文140字)"
           class="w-full max-h-52 resize-none overflow-x-hidden focus:outline-none text-base text-gray-550"
           rows="4"
-          maxlength="140"
         ></textarea>
         <div class="flex justify-between">
-          <span class="text-sm text-gray-500">{{ charCount }}/140</span>
-          <button type="button" @click="addComment" class="text-base leading-5 text-primary-blue">
+          <span class="text-sm text-gray-500" :class="{ 'text-red-500': charCount > maxLength }"
+            >{{ charCount }}/{{ maxLength }}</span
+          >
+          <button
+            type="button"
+            @click="addComment"
+            :disabled="charCount > maxLength"
+            class="text-base leading-5 text-primary-blue disabled:opacity-50"
+          >
             送出留言
           </button>
         </div>
