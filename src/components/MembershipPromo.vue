0b2c2f6b534217ab1a740ff2b78cd612dfd536e1
“WangYingJu” <iriswang1091@gmail.com>
2025-02-26 15:50:46 +0800

    refactor: rename SidebarAd to MembershipPromo and update content.

---
+++ src/components/MembershipPromo.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+import { useToast } from 'vue-toastification'
+
+const toast = useToast()
+
+const isClickApply = ref(false)
+const handleApplyRequest = () => {
+  isClickApply.value = true
+  toast.warning('目前暫停此服務，敬請期待！')
+  setTimeout(() => {
+    isClickApply.value = false
+  }, 1000)
+}
+</script>
+
+<template>
+  <div class="border rounded border-gray-250 bg-white p-5" style="width: 250px; height: 100%">
+    <h2 class="text-lg font-semibold leading-6 mb-4">
+      <span class="font-extrabold text-primary-blue">US plus</span><br />獨家功能解鎖影響力
+    </h2>
+    <p class="text-base leading-6 text-gray-350 mb-4">
+      你的話題值得被看見！升級 US plus，享受更高曝光！
+    </p>
+    <button
+      type="button"
+      class="text-base leading-5 border rounded border-primary-blue bg-white hover:bg-primary-blue text-primary-blue hover:text-white w-full py-2.5 mb-4"
+      :class="{ ' disabled:bg-gray-400 border-gray-400': isClickApply }"
+      :disabled="isClickApply"
+      @click="handleApplyRequest"
+    >
+      立即升級 Plus 會員
+    </button>
+    <p class="text-base leading-5 text-center text-gray-350">快跟上！</p>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import HotTopicQuickAdd from '@/components/HotTopicQuickAdd.vue'
 import HotTopicsList from '@/components/HotTopicsList.vue'
-import SidebarAd from '@/components/SidebarAd.vue'
+import MembershipPromo from '@/components/MembershipPromo.vue'
 import TopicList from '@/components/TopicList.vue'
 import { useRouter } from 'vue-router'
 import { useFormDirty } from '@/stores/useFormDirty'
@@ -29,7 +29,7 @@ onUnmounted(() => {
 
 <template>
   <main class="container container-customizing-1060 flex justify-between my-[30px]">
-    <SidebarAd />
+    <MembershipPromo />
     <TopicList />
     <div class="right-sidebar">
       <HotTopicQuickAdd @navigate="handleNavigate" />
